a02a5cf3ec36887b9b98130c0eb309c257321b8b
Victor Martinez <victor.martinez@lend.tech>
2023-08-02 09:45:32 -0300

    fix: base model should have id, created_at, updated_at

--- chargeapi/db/base_model.py
+++ chargeapi/db/base_model.py
@@ -1,6 +1,23 @@
+import uuid
+
+from sqlalchemy import Column, DateTime
+from sqlalchemy.dialects import postgresql
+
 from chargeapi.db import Base
+from chargeapi.db.functions import utcnow
+
 
 
 class BaseModel(Base):
     # __abstract__ has to be True for Alembic doesn't create BaseModel as a Table in DB
     __abstract__ = True
+
+    id = Column(
+        postgresql.UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4
+    )
+    created_at = Column(DateTime(timezone=True), server_default=utcnow())
+    updated_at = Column(
+        DateTime(timezone=True),
+        server_default=utcnow(),
+        onupdate=utcnow(),
+    )
--- chargeapi/db/models.py
+++ chargeapi/db/models.py
@@ -3,13 +3,12 @@ from sqlalchemy import Column, Date, Integer, String
 from chargeapi.db.base_model import BaseModel
 
 
-class DBBankSlipDebt(BaseModel):
-    __tablename__ = "bank_slip_debts"
+class DBBankSlip(BaseModel):
+    __tablename__ = "bank_slips"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String, nullable=False)
     government_id = Column(String, nullable=False)
     email = Column(String, nullable=False)
     debt_amount = Column(String, nullable=False)
     debt_due_date = Column(Date)
-    debt_id = Column(String, nullable=False)
+    debt_id = Column(String, nullable=False, unique=True)
