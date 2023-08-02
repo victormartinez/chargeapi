7b9ddd1e9bfa09276cf87867c9525a290f19b223
Victor Martinez <victor.martinez@lend.tech>
2023-08-02 09:44:52 -0300

    fix: no return of object when persisting bank slips

--- chargeapi/app/bank_slips/service.py
+++ chargeapi/app/bank_slips/service.py
@@ -3,14 +3,15 @@ from typing import List
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from infrastructure import logging
-from chargeapi.app.bank_slips.data import BankSlipIn, PersistBankSlipsRepository
+from chargeapi.app.bank_slips.data import (
+    BankSlipIn, PersistBankSlipsRepository
+)
 
 
 logger = logging.get_logger(__name__)
 
 
-async def persist(session: AsyncSession, bank_slips: List[BankSlipIn]) -> bool:
+async def persist(session: AsyncSession, bank_slips: List[BankSlipIn]) -> None:
     logger.info("persisting bank slips", total=len(bank_slips))
     repository = PersistBankSlipsRepository(session)
     await repository.execute(bank_slips)
-    return True
