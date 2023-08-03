1879ddde3f99f8e4b294bc73424608f714388b20
Victor Martinez <victor.martinez@lend.tech>
2023-08-03 15:59:15 -0300

    feat: worker responsible for generating bank slips

--- chargeapi/app/debts/data/repository.py
+++ chargeapi/app/debts/data/repository.py
@@ -1,6 +1,6 @@
-from typing import List
+from typing import List, Tuple
 
-from sqlalchemy import select
+from sqlalchemy import select, func
 
 from chargeapi.db.base_repository import BaseRepository
 from chargeapi.db.models import DBDebt, DBBankSlip
@@ -44,11 +44,45 @@ class ListDebtsRepository(BaseRepository):
         ]
 
     async def execute(self, offset: int, limit: int) -> List[DebtOut]:  # type: ignore
-        query = (
-            select(DBDebt)
-            .offset(offset)
-            .limit(limit)
-        )
+        query = select(DBDebt).offset(offset).limit(limit)
         query_result = await self.db_session.execute(query)
         db_rows = query_result.scalars().all()
         return await self._adapt(db_rows)
+
+
+class ListDebtsWithoutBankSlipsRepository(BaseRepository):
+
+    async def _adapt(self, db_bank_slips: List[DBDebt]) -> List[DebtOut]:
+        return [
+            DebtOut(
+                id=db_obj.id,
+                name=db_obj.name,
+                government_id=db_obj.government_id,
+                email=db_obj.email,
+                debt_amount=db_obj.debt_amount,
+                debt_due_date=db_obj.debt_due_date,
+                debt_identifier=db_obj.debt_identifier,
+            ) 
+            for db_obj in db_bank_slips
+        ]
+
+    async def execute(self, offset: int, limit: int) -> Tuple[int, List[DebtOut]]:  # type: ignore
+        query = (
+            select(
+                DBDebt,
+                func.count(DBDebt.id).over()
+            )
+            .join(DBBankSlip, isouter=True)
+            .where(DBBankSlip.id.is_(None))
+            .order_by(DBDebt.debt_due_date.asc())
+            .offset(offset)
+            .limit(limit)
+        )
+        result = await self.db_session.execute(query)
+        rows = result.all()
+        if not rows:
+            return 0, []
+        
+        total_items = rows[0][1]
+        objects = [row[0] for row in rows]
+        return total_items, await self._adapt(objects)
--- chargeapi/exec/script/generate_bank_slips.py
+++ chargeapi/exec/script/generate_bank_slips.py
@@ -1,13 +1,46 @@
-from datetime import date
-
+import asyncio
 import structlog
+from typing import List
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from chargeapi.db.session import get_session
+from chargeapi.app.bank_slips import create_bank_slip
+from chargeapi.app.debts.data import DebtOut
+from chargeapi.app.debts.data.repository import ListDebtsWithoutBankSlipsRepository
 
 
 logger = structlog.get_logger("main")
 
 
+async def create_bank_slips(session: AsyncSession, debts: List[DebtOut]):
+    background_tasks = set()
+    for item in debts:
+        task = asyncio.create_task(
+            create_bank_slip(
+                session, 
+                item.id,
+                item.name,
+                item.email,
+                item.debt_amount,
+                item.debt_due_date
+            )
+        )
+        background_tasks.add(task)
+        task.add_done_callback(background_tasks.discard)
+    return await asyncio.gather(*background_tasks)
+
+
 async def process() -> None:
-    today = date.today()
+    has_next = True
+    OFFSET, LIMIT, PAGE_SIZE = 0, 10, 10
 
-    logger.debug("hello world", today=today)
+    async for session in get_session():
+        while has_next:
+            repository = ListDebtsWithoutBankSlipsRepository(session)
+            total, debts = await repository.execute(OFFSET, LIMIT)
+            if total == 0 or len(debts) == 0:
+                has_next = False
 
+            await create_bank_slips(session, debts)
+            OFFSET += PAGE_SIZE
